cc339b79b8d77521edb4e9498c48ccf38fdf5f29
Einari Naukkarinen <einari.naukkarinen@outlook.com>
2025-01-29 16:27:28 +0000

    feat: install mercantile

--- app/routers/vectors.py
+++ app/routers/vectors.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
-router = APIRouter(tags=["vectors"])
+import mercantile
 
+router = APIRouter(tags=["vectors"])
 
 @router.get("/vectors/{x}/{y}/{z}", tags=["vectors"])
 async def get(x, y, z):
-    return {x, y, z}
+    print(x, y, z)
+    bbox = mercantile.bounds(486, 332, 10) # TODO: use x, y, z
+    return {"bbox": bbox}
